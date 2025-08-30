82d0388f7c33a33cd0e9bad40ba5011ccbceeb5a
Laurné Jones <laurnejones@MacBookPro.lan>
2025-08-30 00:03:33 -0400

    remove ai news

--- ai_in_hr_insights/pages/sources.py
+++
@@ -1,7 +0,0 @@
-# sources.py
-RSS_SOURCES = {
-    "HR Brew": "https://www.hr-brew.com/rss.xml",
-    "AI News": "https://www.artificialintelligence-news.com/feed/",
-    "Stratechery": "https://stratechery.com/feed/",
-    # Add more sources here
-}
